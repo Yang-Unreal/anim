ed68b51124cfe10c6899ec3168698b5e536fd862
Yang <y953159141@gmail.com>
2024-12-21 22:17:44 +0800

    curve-path

--- src/app/curve.tsx
+++ src/app/curve.tsx
@@ -12,14 +12,28 @@ export default function CurveTransition({
   children: React.ReactNode;
 }) {
   // const wrapperRef = useRef<HTMLDivElement>(null!);
-
+  const svgRef = useRef<SVGSVGElement>(null!);
   // Function to generate and assign refs
-
+  const pathRef = useRef<SVGPathElement>(null!);
   const [windowDimensions, setWindowDimensions] = useState({
     width: 0,
     height: 0,
   });
-
+  const { width, height } = windowDimensions;
+  const initialPath = `
+    M0 300 
+    Q${width / 2} 0 ${width} 300
+    L${width} ${height + 300}
+    Q${width / 2} ${height + 600} 0 ${height + 300}
+    L0 0
+  `;
+  const targetPath = `
+    M0 300
+    Q${width / 2} 0 ${width} 300
+    L${width} ${height}
+    Q${width / 2} ${height} 0 ${height}
+    L0 0
+  `;
   useEffect(() => {
     const handleResize = () => {
       setWindowDimensions({
@@ -40,18 +54,13 @@ export default function CurveTransition({
     };
   }, []);
 
-  interface SVGProps {
-    width: number;
-    height: number;
-  }
-  const SVG = ({ width, height }: SVGProps) => {
-    const initialPath = `
-M0 300
-Q${width / 2}
-`;
+  const SVG = () => {
     return (
-      <svg>
-        <path d={initialPath}></path>
+      <svg
+        ref={svgRef}
+        className="fixed w-screen h-[calc(100dvh+600px)] left-0 top-[-300px] pointer-events-none invisible "
+      >
+        <path ref={pathRef} d={initialPath} />
       </svg>
     );
   };
@@ -67,6 +76,22 @@ Q${width / 2}
         //     duration: 0.5,
         //   }
         // );
+        animate(
+          svgRef.current,
+          { y: ["100vh", 0], visibility: ["visible"] },
+          {
+            duration: 0.5,
+            onComplete: next,
+          }
+        );
+        animate(
+          pathRef.current,
+          { d: [initialPath] },
+          {
+            duration: 0.5,
+            onComplete: next,
+          }
+        );
       }}
       enter={(next) => {
         // animate(
@@ -76,10 +101,27 @@ Q${width / 2}
         //     duration: 0.5,
         //   }
         // );
+        animate(
+          svgRef.current,
+          { y: [0, "-100vh"] },
+          {
+            duration: 0.5,
+            onComplete: next,
+          }
+        );
+        animate(
+          pathRef.current,
+          { d: targetPath },
+          {
+            duration: 0.5,
+            onComplete: next,
+          }
+        );
       }}
     >
+      <div>{windowDimensions.width > 0 && <SVG />}</div>
       <div>{children}</div>
-      <div>{windowDimensions.width > 0 && <SVG {...windowDimensions} />}</div>
+
       <Debug />
     </TransitionRouter>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
-import StairTransition from "@/app/stair"; // “
+// import StairTransition from "@/app/stair";
 // import Providers from "@/app/providers";
 // import { Debug } from "@/app/components/debug";
+import CurveTransition from "@/app/curve";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,7 +32,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* <Providers> */}
-        <StairTransition>{children}</StairTransition>
+        {/* <StairTransition> */}
+        <CurveTransition> {children}</CurveTransition>
+
+        {/* </StairTransition> */}
         {/* </Providers> */}
       </body>
     </html>
