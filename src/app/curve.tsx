2e9babbe93f6e25e7e8540fbd5b660f23f837cb2
Yang <y953159141@gmail.com>
2024-12-22 08:26:52 -0800

    curve-text

--- src/app/curve.tsx
+++ src/app/curve.tsx
@@ -5,16 +5,27 @@ import { TransitionRouter } from "next-transition-router";
 import { animate } from "motion/react"; // Ensure you have motion installed
 import { Debug } from "./components/debug";
 import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
+type Routes = {
+  "/": string;
+  "/about": string;
+};
 export default function CurveTransition({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const routes: Routes = {
+    "/": "Home",
+    "/about": "About",
+  };
+
   // const wrapperRef = useRef<HTMLDivElement>(null!);
   const svgRef = useRef<SVGSVGElement>(null!);
   // Function to generate and assign refs
   const pathRef = useRef<SVGPathElement>(null!);
+  const paraRef = useRef<HTMLParagraphElement>(null!);
   const [windowDimensions, setWindowDimensions] = useState({
     width: 0,
     height: 0,
@@ -22,18 +33,25 @@ export default function CurveTransition({
   const { width, height } = windowDimensions;
   const initialPath = `
     M0 300 
+    Q${width / 2} 300 ${width} 300
+    L${width} ${height + 300}
+    Q${width / 2} ${height + 600} 0 ${height + 300}
+    L0 0
+  `;
+  const leaveTargetPath = `
+    M0 300
     Q${width / 2} 0 ${width} 300
     L${width} ${height + 300}
     Q${width / 2} ${height + 600} 0 ${height + 300}
     L0 0
   `;
-  const targetPath = `
-    M0 300
-    Q${width / 2} 0 ${width} 300
-    L${width} ${height}
-    Q${width / 2} ${height} 0 ${height}
-    L0 0
-  `;
+  const enterTargetPath = `
+  M0 300
+  Q${width / 2} 0 ${width} 300
+  L${width} ${height + 300}
+  Q${width / 2} ${height + 300} 0 ${height + 300}
+  L0 0
+`;
   useEffect(() => {
     const handleResize = () => {
       setWindowDimensions({
@@ -53,14 +71,15 @@ export default function CurveTransition({
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-
+  const pathname = usePathname();
+  console.log(pathname);
   const SVG = () => {
     return (
       <svg
         ref={svgRef}
         className="fixed w-screen h-[calc(100dvh+600px)] left-0 top-[-300px] pointer-events-none invisible "
       >
-        <path ref={pathRef} d={initialPath} />
+        <path ref={pathRef} />
       </svg>
     );
   };
@@ -80,15 +99,16 @@ export default function CurveTransition({
           svgRef.current,
           { y: ["100vh", 0], visibility: ["visible"] },
           {
-            duration: 0.5,
-            onComplete: next,
+            duration: 0.75,
+            ease: [0.76, 0, 0.24, 1],
           }
         );
         animate(
           pathRef.current,
-          { d: [initialPath] },
+          { d: [initialPath, leaveTargetPath] },
           {
-            duration: 0.5,
+            duration: 0.75,
+            ease: [0.76, 0, 0.24, 1],
             onComplete: next,
           }
         );
@@ -105,15 +125,16 @@ export default function CurveTransition({
           svgRef.current,
           { y: [0, "-100vh"] },
           {
-            duration: 0.5,
-            onComplete: next,
+            duration: 0.75,
+            ease: [0.76, 0, 0.24, 1],
           }
         );
         animate(
           pathRef.current,
-          { d: targetPath },
+          { d: enterTargetPath },
           {
-            duration: 0.5,
+            duration: 0.75,
+            ease: [0.76, 0, 0.24, 1],
             onComplete: next,
           }
         );
@@ -121,6 +142,13 @@ export default function CurveTransition({
     >
       <div>{windowDimensions.width > 0 && <SVG />}</div>
       <div>{children}</div>
+
+      <p
+        className="text-xl font-semibold text-gray-800 absolute pointer-events-none inset-0 "
+        ref={paraRef}
+      >
+        {routes[pathname as keyof Routes]}
+      </p>
 
       <Debug />
     </TransitionRouter>
