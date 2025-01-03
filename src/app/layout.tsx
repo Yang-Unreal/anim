f55f90f60c0306bd9389e74dd27e07ec0e85a502
Yang <y953159141@gmail.com>
2025-01-02 23:16:11 -0800

    lenis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ReactLenis } from "@/lib/utils/lenis";
 import CurveTransition from "@/components/curve/curve";
 
 const geistSans = Geist({
@@ -29,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <CurveTransition> {children} </CurveTransition>
+        <ReactLenis root>
+          <CurveTransition>{children} </CurveTransition>
+        </ReactLenis>
       </body>
     </html>
   );
