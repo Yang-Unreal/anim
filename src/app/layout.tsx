9f0881f7fffd7dde1b13360849db03570f9feb82
Yang <y953159141@gmail.com>
2025-01-19 21:25:32 -0800

    test-4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout({
       >
         <ReactLenis root>
           <CurveTransition>
-            {children}
             <MenuProvider>
+              {children}
               <Header />
             </MenuProvider>
             <Debug />
