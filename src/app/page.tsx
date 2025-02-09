37748535f626f10e033c4c802d6ed0ab043bf207
unknown <y953159141@gmail.com>
2025-02-09 17:31:33 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,21 @@ import Description from "@/components/description/description";
 import SlidingImages from "@/components/slidingImages/slidingImages";
 import Contact from "@/components/contact/contact";
 import { usePreloaderContext } from "@/components/provider/preloaderContextProvider";
+
 export default function Home() {
   const { showPage } = usePreloaderContext();
+
   return (
-    <main className={`${!showPage ? "opacity-0" : "opacity-100"} `}>
-      <Landing />
-      <Description />
-      <ProjectGallery initialProjects={projects} />
-      <SlidingImages />
-      <Contact />
-    </main>
+    <>
+      {showPage && (
+        <main>
+          <Landing />
+          <Description />
+          <ProjectGallery initialProjects={projects} />
+          <SlidingImages />
+          <Contact />
+        </main>
+      )}
+    </>
   );
 }
